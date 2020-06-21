23090247f0c4eda597bf9da92c339c500321ead3
Kharanshu Dewan <63278591+Kharanshu31@users.noreply.github.com>
2020-06-21 16:22:24 +0530

    Day 19

--- PermutationSequence.cpp
+++ PermutationSequence.cpp
@@ -2,29 +2,6 @@ class Solution {
 public:
     
     string getPermutation(int N, int K) {
-        
-//         int x,y;
-//         vector<int>fact = {1,1,2,6,24,120,720,5040,40320,362880};
-       
-//         vector<int> num(n);
-        
-//         for(int i=0; i<n; i++)
-//             num[i] = i+1;
-        
-//         string s="";
-//         while(n>=0)
-//         {
-//             x=k/fact[n-1];
-//             y=k%fact[n-1];
-            
-//             s+=(num[x]+'0');
-//             num.erase(num.begin()+x);
-            
-//             n--;
-//             k=y;
-//         }
-        
-//         return s;
     
         int n = N-1, k = K-1, nt, kt;
         
