42e02b068e3324125f6b79e4e55c04052d8986de
Kharanshu Dewan <63278591+Kharanshu31@users.noreply.github.com>
2020-06-26 17:31:06 +0530

    Day 23

---
+++ UniqueBST.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    
+    int numTrees(int n) {
+        vector<int>dp(n+1,0);
+         dp[0] = dp[1] = 1;
+        for(int i = 2; i < n + 1; i++){
+            for(int j = 0; j < i; j++){
+                dp[i] += (dp[j] * dp[i - j - 1]);
+            }
+        }
+        
+        return dp[n];
+        
+    }
+};
